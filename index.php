ac3292015b00f8610ae34b8dc49e6dd427c61695
James Towers <james@songdrop.com>
2017-02-18 16:51:40 +0000

    new images and copy

--- index.php
+++ index.php
@@ -4,30 +4,35 @@
 
 		<div class="pad group gradient-border-bottom">
 			<img src="library/images/kit_jersey_front.png" class="kit-photo col6" />
-			<img src="library/images/kit_bibs_front.png" class="kit-photo col6" />
+			<img src="library/images/kit_jersey_back.png" class="kit-photo col6" />
 		</div>
 
 		<div class="pad group" id="buy-the-kit">
 			
-			<p>Introducing the first cycling kit from Je Suis Cool…. The Pina Colada Kit. 
-			Dreamt up whilst drunk… designed whilst still drunk. If you want a piece of the action 
-			then get involved in the pre-order below. Each one is lovingly handmade to the highest 
-			quality by genuine London cool guys <a href=" http://milltag.cc">Milltag</a>. They’re avaliable in sizes S - XXL in both 
-			men’s and women’s cuts. SO FRESH. Now this is a pre-order, so once the order closes i 
-			will put the order in to Milltag, and you can expect your delivery in 6-8 weeks. Now 
-			go get sweaty.</p>
+			<p>Je Suis Cool introduces SPACE LUXÉ. High-performance sports wear that looks good enough to wear into outer space. Buzz Armstrong lost his shit when he saw this and he won’t be the last spaceman to drop the f-bomb when they lay eyes on the new collection…</p>
 
 			<p>&nbsp;</p>
 
-			<h1>Pre orders are now closed...</h1> 
-			<p>If you’re interested in future runs of this kit please <a href="mailto:harry@harryosborne.co.uk">email me here</a></p>
+			<h2>We give you.</h2>
+			<h1>JE SUIS COSMIC.</h1>
 
-			<?php /*
-			<div class="col4 text-center buy-button">	
-				<a href="https://www.paypal.com/cgi-bin/webscr?business=harry@harryosborne.co.uk&cmd=_xclick&currency_code=GBP&amount=60&item_name=Je%20Suis%20Cool%20Pina%20Colada%20Jersey" class="btn btn-full-width" target="_blank">Pre-Order Jersey</a>
+			<p>The first interstellar outfit from the makers of the Pina Colada Kit.</p> 
+
+			<p>If you want a piece of the action then get involved in the pre-order below. Each one is lovingly handmade to the highest quality by genuine London cool guys Milltag. They’re avaliable in sizes S - XXL in both men’s and women’s cuts. SO COSMIC. This is a pre-order, so once the order closes i will put the order in to Milltag, and you can expect your delivery in 6-8 weeks.<a href="mailto:harry@harryosborne.co.uk">email me here</a></p>
+	
+			<p>- Jerseys are £78 delivered (please add £10 for international delivery)
+			To pre order the kit, use this button to make a paypal payment. Please include your size and delivery address in the notes</p>
+			
+			<div class="text-center buy-button">	
+				<a href="https://www.paypal.com/cgi-bin/webscr?business=harry@harryosborne.co.uk&cmd=_xclick&currency_code=GBP&amount=78&item_name=Je%20Suis%20Cool%20Pina%20Colada%20Jersey" class="btn btn-full-width" target="_blank">Pre-Order Jersey</a>
 				<span>£60 delivered</span>
 			</div>
 
+			<p>- Pre-order closes midnight 19th February. Kits will be delivered Mid April</p>
+
+			<p>- lego spaceman zipper detail and patch image to replace the pineapple and bib shorts</p>
+			
+			<?php /*
 			<div class="col4 text-center buy-button">	
 				<a href="https://www.paypal.com/cgi-bin/webscr?business=harry@harryosborne.co.uk&cmd=_xclick&currency_code=GBP&amount=65&item_name=Je%20Suis%20Cool%20Pina%20Colada%20Bibs" class="btn btn-full-width" target="_blank">Pre-Order Bibs</a>
 				<span>£65 delivered</span>
@@ -37,31 +42,32 @@
 				<a href="https://www.paypal.com/cgi-bin/webscr?business=harry@harryosborne.co.uk&cmd=_xclick&currency_code=GBP&amount=120&item_name=Je%20Suis%20Cool%20Pina%20Colada%20Full%20Kit" class="btn btn-full-width" target="_blank">Pre-Order Whole Kit</a>
 				<span>£120 delivered</span>
 			</div>
+			*/ ?>
 			<p>&nbsp;</p>
 			<p><strong><em>* Please add your <a href="http://milltag.cc/information" target="_blank">size info</a> and delivery address to the notes on the order</em></strong></p>
 			<p><strong><em>** Add £10 for international delivery</em></strong></p>
-			*/?>
+			
 		</div>
 
 		<div class="gradient-border-bottom group pad">
-			<img src="library/images/kit_jersey_back.png" class="kit-photo col5" />
-			<img src="library/images/kit_zip.png" class="kit-photo col2" align="bottom" style="max-width:130px"/>
-			<img src="library/images/kit_bibs_side.png" class="kit-photo col5" />
-			<img src="library/images/kit_collar_detail.png" class="kit-photo col12" />
-			<div class="text-center small"><em>Inside collar detail</em></div>
+			<div class="col6">
+				<img src="library/images/kit_zip.png" class="kit-photo" align="bottom" style="max-width:130px"/>
+				<div class="text-center small"><em>The zipper</em></div>
+			</div>
+			<div class="col6">
+				<img src="library/images/patch.png" class="kit-photo" style="max-width:230px" />
+				<div class="text-center small"><em>The patch</em></div>
+			</div>
 		</div>
 
 		<div class="pad group gradient-border-bottom" id="details">
 
-			<p> The jerseys feature Lycra ‘Power Cuffs’, elastane collar and sleeves for increased 
-		aerodynamics (under-rated), full hidden zip, silicone gripper strip on hem, three rear 
-		pockets with extra hidden zip pocket and flat-lock seams for comfort. POW. The bibs are 
-		just as special, featuring a chamois pad so comfy you’ll wear it to bed and silicone 
-		leg grippers to keep your junk tidy. (well, so the reviews say!).... You can see more 
-		over at Milltag, but what they won;t tell you is how  I’m going to lovingly hand attach 
-		a gold pineapple to each zipper pull. Cos you know… pineapples.</p>
+			<h2> Heres the science:</h2>
+			<p>The jersey will be made by original space cadets Milltag, in both men’s and women’s cuts. It has all the features you’d expect of a pressure controlled space-suit, including silicone gripper hems, concealed full length zip and 3 pockets to the rear for “Space Cakes”. There is also a hidden zipped pocket for cash, keys to the space rocket and any samples of moon-dust you may keep about your person… We’re looking at you Lance!</p>
 
-			<iframe width="100%" height="415" src="https://www.youtube.com/embed/w5_EIikdFr8" frameborder="0" allowfullscreen></iframe>
+			<p>his time we’ve gone with the club fit. It’s exactly the same quality as the Pina Colada Collection, but with slightly longer sleeves and a slightly tighter cut. If you want more info, check out: <a href="http://milltag.cc">Milltag.cc</a></p>
+
+			<iframe width="100%" height="415" src="https://www.youtube.com/embed/qORYO0atB6g" frameborder="0" allowfullscreen></iframe>
 
 		</div>
 
@@ -72,10 +78,7 @@
 
 			<h2>About Je Suis Cool</h2>
 
-			<p>Je Suis Cool is SPORT LUXÉ… It’s like Nike meets Prada. It’s technical gear that looks so fucking good you’ll want to wear it out to party. It’s like a brand new pair of gucci loafers for your sweaty gooch.
-			<p>We’ve all spent too long pondering “if only someone would make lycra good enough that i could wear it to an important bizniz conference”… Well now some one does: JE SUIS COOL.</p>
-			<p>Type that into google... Nothing happens.</p>
-			<p>But seriously… if you want lycra that looks as good poolside, pina colada in hand as it does on the podium of the local criterium series, then you’re in the right place.</p>
+			<p>Je Suis Cool is the UK's laziest cycling club. Whilst we push the boundaries of acceptable lycra, we rarely get close to the boundaries of on the bike performance. We’re never afraid to sack off the ride in favour of a swim or spend so long in the pub that it’s no longer possible to cycle home. If that sounds like you, or you’re the kind of cyclist that would un-prompted suggest getting the train home, then welcome to the team.</p>
 			<p>If you want to know more... or just want to chat... <a href="mailto:harry@harryosborne.co.uk">hit me up!</a></p>
 
 		</div>
